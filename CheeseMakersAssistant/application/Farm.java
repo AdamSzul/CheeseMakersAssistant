ce27f372e0f5abc28d2efb970bbcbd21ad1dc354
mmhenry2 <47957178+mmhenry2@users.noreply.github.com>
2020-04-29 14:01:49 -0500

    Update Farm.java

--- CheeseMakersAssistant/application/Farm.java
+++ CheeseMakersAssistant/application/Farm.java
@@ -17,21 +17,21 @@ public class Farm {
   }
   
   /**
-	 * sets the given weight at the given date, replaces previous weight if date already exists
-	 * 
-	 * @param date
-	 * @param weight
-	 */
+  * sets the given weight at the given date, replaces previous weight if date already exists
+  * 
+  * @param date
+  * @param weight
+  */
   public void insert(GregorianCalendar date, int weight){
     shipments.put(date, weight);
   }
   
   /**
-	 * gets the weight for given date, returns 0 if date doesn't exist
-	 * 
-	 * @param date
-	 * @return weight
-	 */
+  * gets the weight for given date, returns 0 if date doesn't exist
+  * 
+  * @param date
+  * @return weight
+  */
   public int get(GregorianCalendar date) {
     if (shipments.containsKey(date))
       return shipments.get(date);
@@ -40,61 +40,61 @@ public class Farm {
   }
   
   /**
-	 * gets the list of weights within the given range
-	 * returns empty list if none within range or if farm is empty
-	 * 
-	 * @param start
-	 * @param end
-	 * @return weights in range
-	 */
-	public List<Integer> forRange (GregorianCalendar start, GregorianCalendar end) {
-		List<Integer> inRange = new ArrayList<Integer>();
-		Set<GregorianCalendar> allDates = shipments.keySet();
+  * gets the list of weights within the given range
+  * returns empty list if none within range or if farm is empty
+  * 
+  * @param start
+  * @param end
+  * @return weights in range
+  */
+  public List<Integer> forRange (GregorianCalendar start, GregorianCalendar end) {
+    List<Integer> inRange = new ArrayList<Integer>();
+    Set<GregorianCalendar> allDates = shipments.keySet();
 		
-		for (GregorianCalendar d : allDates) {
-			if (d.compareTo(start) >= 0 && d.compareTo(end) <= 0) { // checks if in range
-				inRange.add(shipments.get(d));
-			}
-		}
+      for (GregorianCalendar d : allDates) {
+        if (d.compareTo(start) >= 0 && d.compareTo(end) <= 0) { // checks if in range
+	  inRange.add(shipments.get(d));
+        }
+      }
 		
-		return inRange;
-	}
+    return inRange;
+  }
   
   /**
-	 * gets the list of weights for the month
-	 * 
-	 * @param year
-	 * @param month
-	 * @return weights for month
-	 */
-	public List<Integer> forMonth (int year, int month){
-		List<Integer> inRange = new ArrayList<Integer>();
-		Set<GregorianCalendar> allDates = shipments.keySet();
+  * gets the list of weights for the month
+  * 
+  * @param year
+  * @param month
+  * @return weights for month
+  */
+  public List<Integer> forMonth (int year, int month) {
+    List<Integer> inRange = new ArrayList<Integer>();
+    Set<GregorianCalendar> allDates = shipments.keySet();
 		
-		for (GregorianCalendar d : allDates) {
-			if (d.get(GregorianCalendar.MONTH) == month && d.get(GregorianCalendar.YEAR) == year) { // checks if in range
-				inRange.add(shipments.get(d));
-			}
-		}
+    for (GregorianCalendar d : allDates) {
+      if (d.get(GregorianCalendar.MONTH) == month && d.get(GregorianCalendar.YEAR) == year) { // checks if in range
+	inRange.add(shipments.get(d));
+      }
+    }
 		
-		return inRange;
-	}
+    return inRange;
+  }
   
   /**
-	 * gets the list of weights for the year
-	 * 
-	 * @param year
-	 * @return list for year
-	 */
-	public List<Integer> forYear (int year){
-		List<Integer> inRange = new ArrayList<Integer>();
-		Set<GregorianCalendar> allDates = shipments.keySet();
+  * gets the list of weights for the year
+  * 
+  * @param year
+  * @return list for year
+  */
+  public List<Integer> forYear (int year) {
+    List<Integer> inRange = new ArrayList<Integer>();
+    Set<GregorianCalendar> allDates = shipments.keySet();
 		
-		for (GregorianCalendar d : allDates) {
-			if (d.get(GregorianCalendar.YEAR) == year)
-				inRange.add(shipments.get(d));
-		}
+    for (GregorianCalendar d : allDates) {
+      if (d.get(GregorianCalendar.YEAR) == year)
+        inRange.add(shipments.get(d));
+    }
 		
-		return inRange;
-	}
+     return inRange;
+  }
 }
