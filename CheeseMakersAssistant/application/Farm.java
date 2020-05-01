14a72e80e24f17fa18d0880a5f2f9b98afcb3949
Adam <aaszulczewsk@wisc.edu>
2020-04-30 19:56:24 -0500

    fixed farm insert

--- CheeseMakersAssistant/application/Farm.java
+++ CheeseMakersAssistant/application/Farm.java
@@ -4,45 +4,44 @@ import java.util.*;
 
 
 /**
- * Stores and provides access to data for all the milk deliveries for
+ * Stores and provides access to data for all the milk deliveries for 
  * a single farm in a HashMap
+ *
  */
 public class Farm {
   private HashMap<GregorianCalendar, Integer> shipments;
-
+  
   public Farm() {
     shipments = new HashMap<>();
   }
-
+  
   /**
-   * sets the given weight at the given date, replaces previous weight if date already exists
-   *
-   * @param date
-   * @param weight
-   */
-  public void insert(GregorianCalendar date, int weight) {
-    shipments.put(date, weight);
+  * sets the given weight at the given date, replaces previous weight if date already exists
+  * 
+  * @param date
+  * @param weight
+  */
+  public void insert(GregorianCalendar date, int weight){
+    if (weight != 0)
+      shipments.put(date, weight);
+    else
+      shipments.remove(date);
   }
-
+  
   /**
-   * gets the weight for given date, returns 0 if date doesn't exist
-   *
-   * @param date
-   * @return weight
-   */
+  * gets the weight for given date, returns 0 if date doesn't exist
+  * 
+  * @param date
+  * @return weight
+  */
   public int get(GregorianCalendar date) {
     if (shipments.containsKey(date))
       return shipments.get(date);
     else
       return 0;
   }
-
-  /**
-   * Get the shipments map
-   *
-   * @return the shipments
-   */
-  public Map<GregorianCalendar, Integer> getShipments() {
+  
+  public HashMap<GregorianCalendar, Integer> getShipments() {
     return shipments;
   }
 
@@ -51,13 +50,13 @@ public class Farm {
    * returns empty list if none within range or if farm is empty
    *
    * @param startDate the start date
-   * @param endDate   the end date
+   * @param endDate the end date
    * @return weights in range
    */
   public List<Integer> forRange(GregorianCalendar startDate, GregorianCalendar endDate) {
     List<Integer> inRange = new ArrayList<>();
     GregorianCalendar date = (GregorianCalendar) startDate.clone();
-    while (date.compareTo(endDate) <= 0) {
+    while(date.compareTo(endDate) <= 0) {
       inRange.add(get(date));
       date.add(Calendar.DAY_OF_MONTH, 1);
     }
